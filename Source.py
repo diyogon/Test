07f1585011a0a95569279f560af188a7e9196aa2
Noel Vincent <Noel@noels-mbp.dhcp.nd.edu>
2018-11-27 14:59:58 -0500

    setting up tests

--- Source.py
+++ Source.py
@@ -1,2 +1,13 @@
 def add(a,b):
   return a+b
+def isTop(point):
+  if point[1] >= 600:
+    return False
+  else: 
+    return True
+def isLeft(point):
+  if point[0] <= 350:
+    return True
+  else:
+    return False
+ 
--- Tests.py
+++ Tests.py
@@ -5,3 +5,11 @@ dst_points = [[85, 1163], [686, 1163], [686, 77], [82, 64], [82, 226], [244, 64]
 
 def test_initial():
   assert dst_points[:] == dst_points[:]
+
+def test_top():
+  assert Source.isTop([100,100])
+  assert not Source.isTop([100,700])
+
+def test_left():
+  assert Source.isLeft([100,100])
+  assert not Source.isLeft([400,100])
